33582479349481c0e01efdaba353886a561fa19a
sk8beemo <skateor2007@gmail.com>
2020-09-07 00:00:49 +0300

    Simple Logic, transform question.

--- src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import classes from './AnswerItem.module.css'
 
-const AnswerItem = props => (
-    <li className={classes.AnswerItem}>
+const AnswerItem = props => {
+    const cls = [classes.AnswerItem]
+
+    if (props.state) {
+        cls.push(classes[props.state])
+    }
+
+    return (<li className={cls.join(' ')}
+        onClick={()=>props.onAnswerClick(props.answer.id)}>
         {props.answer.text}
-    </li>
-)
+    </li>)
+}
 
 export default AnswerItem
