02ee1b5149939ada214762700902aed477d4cb14
sk8beemo <skateor2007@gmail.com>
2020-09-09 13:58:15 +0300

    Finish Practice Component

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends React.Component {
 
     state ={
+        results: {},
+        isFinished: false,
         activeQuestion: 0,
         answerState: null,
         quiz: [
@@ -34,31 +37,45 @@ class Quiz extends React.Component {
     }
 
     onAnswerClickHandler = (answerId) => {
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if (this.state.answerState[key] === 'success') {
+                return
+            }
+        }
+
         const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
 
         if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
 
             this.setState({
-                answerState: {[answerId]: 'success'}
+                answerState: {[answerId]: 'success'},
+                results
             })
 
             const timeout = window.setTimeout(()=>{
 
                 if (this.isQuizFinished()) {
-                    console.log('Finished')
+                    this.setState({
+                        isFinished: true
+                    })
                 } else {
                     this.setState({
                         activeQuestion: this.state.activeQuestion + 1,
                         answerState: null,
                     })
                 }
-
                 window.clearTimeout(timeout)
-
             }, 1000)
         } else {
+            results[question.id] = 'error'
             this.setState({
-                answerState: {[answerId]: 'error'}
+                answerState: {[answerId]: 'error'},
+                results
             })
         }
     }
@@ -67,20 +84,36 @@ class Quiz extends React.Component {
         return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
+        })
+    }
     render() {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
                     <h1>Ответить на все вопросы</h1>
 
-                    <ActiveQuiz
-                        answers={this.state.quiz[this.state.activeQuestion].answers}
-                        question={this.state.quiz[this.state.activeQuestion].question}
-                        onAnswerClick={this.onAnswerClickHandler}
-                        quizLength={this.state.quiz.length}
-                        answerNumber={this.state.activeQuestion +1}
-                        state={this.state.answerState}
-                    />
+                    {
+                        this.state.isFinished
+                        ? <FinishedQuiz
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+                            />
+                            : <ActiveQuiz
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                question={this.state.quiz[this.state.activeQuestion].question}
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLength={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion +1}
+                                state={this.state.answerState}
+                            />
+                    }
                 </div>
             </div>
         )
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import classes from './Layout.module.css'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
 
 class Layout extends React.Component {
+
+    state = {
+        menu: false
+    }
+
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render() {
         return(
             <div className={classes.Layout}>
+
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.menuCloseHandler}
+                />
+
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     {this.props.children}
                 </main>
