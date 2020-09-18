f4347dc9b028c9340b10dcea0a66091ee4277f40
sk8beemo <skateor2007@gmail.com>
2020-09-18 13:04:21 +0300

    Add User Authorization

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,133 @@
 import React from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from "../../components/UI/Input/Input";
+import is from 'is_js'
 
 export default class Auth extends React.Component {
+
+    state = {
+      isFormValid: false,
+      formControls: {
+          email: {
+              value: '',
+              type: 'email',
+              label: 'Email',
+              errorMesage: 'Введите валидный email',
+              valid: false,
+              touched: false,
+              validation: {
+                  required: true,
+                  email: true
+              }
+          },
+          password: {
+              value: '',
+              type: 'password',
+              label: 'Пароль',
+              errorMesage: 'Неверный пароль',
+              valid: false,
+              touched: false,
+              validation: {
+                  required: true,
+                  minLength: 6
+              }
+          }
+      }
+    }
+
+    loginHandler = () => {
+    }
+
+    registerHandler = () => {
+    }
+
+    submitHandler = () => {
+    }
+
+    validateControl(value, validation) {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true
+
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+        if (validation.email) {
+            isValid = is.email(value) && isValid
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+        return isValid
+    }
+
+    onChangeHandler = (event, controlName) => {
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls, isFormValid
+        })
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            console.log(control)
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!control.validation}
+                    errorMessage={control.errorMessage}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+
+                        {this.renderInputs()}
+
+                        <Button
+                            type="success"
+                            onclick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
+                        >
+                            Войти</Button>
+                        <Button
+                            type="primary"
+                            onclick={this.registerHandler}
+                            disabled={!this.state.isFormValid}
+                        >Зарегистрироваться</Button>
+                    </form>
+                </div>
             </div>
         )
     }
